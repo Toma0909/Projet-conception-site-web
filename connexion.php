c935fe3a6506aeb10291b01cb19681747308f626
Thomas <thomas.cole@groupe-esigelec.org>
2025-10-15 16:42:23 +0200

    page connexion

---
+++ connexion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Connexion</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Connexion</h1>
+    <nav>
+      <ul>
+        <li><a href="index.html">Accueil</a></li>
+        <li><a href="inscription.html">Inscription</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <form action="#" method="post">
+      <label for="email">Email :</label>
+      <input type="email" id="email" name="email" required>
+
+      <label for="mdp">Mot de passe :</label>
+      <input type="password" id="mdp" name="mdp" required>
+
+      <button type="submit">Se connecter</button>
+    </form>
+  </main>
+</body>
+</html>
