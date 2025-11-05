52b37f0f122e0b44d45296657dc4c63d1acd4751
Kenzouille <92575631+Kenzyz76@users.noreply.github.com>
2025-11-05 14:00:40 +0100

    Base pour commencer le projet

--- connexion.php
+++ connexion.php
@@ -1,31 +1,28 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <title>Connexion</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <header>
-    <h1>Connexion</h1>
-    <nav>
-      <ul>
-        <li><a href="index.html">Accueil</a></li>
-        <li><a href="inscription.html">Inscription</a></li>
-      </ul>
-    </nav>
-  </header>
+<?php
+  $titre = "Connexion";
 
-  <main>
-    <form action="#" method="post">
-      <label for="email">Email :</label>
-      <input type="email" id="email" name="email" required>
+  include('header.inc.php');
+  include('menu.inc.php');
+?>
 
-      <label for="mdp">Mot de passe :</label>
-      <input type="password" id="mdp" name="mdp" required>
+  <h1>Connexion à votre compte</h1>
+  <form  method="POST" action="tt_connexion.php">
+    <div class="row my-3">
+      <div class="col-md-6">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control " id="email" name="email" placeholder="Votre email..." required>
+      </div>
+      <div class="col-md-6">
+        <label for="password" class="form-label">Mot de passe</label>
+        <input type="password" class="form-control " id="password" name="password" placeholder="Votre mot de passe..." required>
+      </div>
+    </div>
+    <div class="row my-3">
+      <div class="d-grid d-md-block">
+      <button class="btn btn-outline-primary" type="submit">Connexion</button></div>   
+    </div>
 
-      <button type="submit">Se connecter</button>
-    </form>
-  </main>
-</body>
-</html>
+  </form>
+<?php
+  include('footer.inc.php');
+?>
--- inscription.php
+++ inscription.php
@@ -1,34 +1,43 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <title>Inscription</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <header>
-    <h1>Inscription</h1>
-    <nav>
-      <ul>
-        <li><a href="index.html">Accueil</a></li>
-        <li><a href="connexion.html">Connexion</a></li>
-      </ul>
-    </nav>
-  </header>
+<?php
+  $titre = "Inscription";
 
-  <main>
-    <form action="#" method="post">
-      <label for="nom">Nom :</label>
-      <input type="text" id="nom" name="nom" required>
+  include('header.inc.php');
+  include('menu.inc.php');
+?>
 
-      <label for="email">Email :</label>
-      <input type="email" id="email" name="email" required>
+  <h1>Création d'un compte</h1>
+  <form  method="POST" action="tt_inscription.php">
 
-      <label for="mdp">Mot de passe :</label>
-      <input type="password" id="mdp" name="mdp" required>
-
-      <button type="submit">S'inscrire</button>
-    </form>
-  </main>
-</body>
-</html>
+    <!-- Nom et prénom sur une ou deux lignes (en fonction de la taille) -->
+    <!-- Ici on utilise le breakpoint medium -->
+    <div class="row my-3">
+        <div class="col-md-6">
+            <label for="nom" class="form-label">Nom</label>
+            <input type="text" class="form-control " id="nom" name="nom" placeholder="Votre nom..." required>
+        </div>
+        <div class="col-md-6">
+            <label for="prenom" class="form-label">Prénom</label>
+            <input type="text" class="form-control " id="prenom" name="prenom" placeholder="Votre prénom..." required>
+        </div>
+    </div>
+    <!-- email et mot de passe -->
+    <div class="row">
+        <div class="col-md-6">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control " id="email" name="email" placeholder="Votre email..." required>
+        </div>
+        <div class="col-md-6">
+            <label for="password" class="form-label">Mot de passe</label>
+            <input type="password" class="form-control " id="password" name="password" placeholder="Votre mot de passe..." required>
+        </div>
+    </div>
+    <div class="row my-3">
+        <div class="d-grid d-md-block">
+            <button class="btn btn-outline-primary" type="submit">Inscription</button></div>   
+        </div>
+    </div>
+</form>
+ 
+<?php
+  include('footer.inc.php');
+?>
